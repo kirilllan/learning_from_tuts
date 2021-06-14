246f085cf9c729b18bfb1a1d38e399a7cc7be4a4
kirilllan <s2100138@edu.bc.fi>
2021-06-14 15:12:46 +0300

    banner styles

--- movie-search-app/src/Banner.js
+++ movie-search-app/src/Banner.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import baseURL from "./axios";
 import requests from "./requests";
+import "./Banner.css";
 
 function Banner() {
   const [movie, setMovie] = useState([]);
@@ -9,7 +10,8 @@ function Banner() {
     async function updateBanner() {
       const request = await baseURL.get(requests.fetchNetflixOriginals);
       const moviesData = request.data.results;
-      setMovie[moviesData[Math.floor(Math.random() * moviesData.length)]];
+      setMovie(moviesData[Math.floor(Math.random() * moviesData.length)]);
+      console.log('MOVIE, ', movie)
       return request;
     }
     updateBanner();
@@ -20,22 +22,21 @@ function Banner() {
       <div className="banner"
         style={{backgroundSize: "cover",
         backgroundImage: `url(
-          "https://image.tmdb.org/t/p/original/${movie?.backgrop_path}"
+          "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
         )`,
         backgroundPosition: "center center"}}>
         <div className="banner__contents">
-          <h1>
+          <h1 className="banner__title">
             {movie?.title || movie?.name || movie?.original_name}
             <div className="banner__buttons">
               <button className="banner__button">Play</button>
               <button className="banner__button">My List</button>
             </div>
-          </h1>
-        </div>
-
+          </h1> 
         <h1 className="banner__description">
-          {movie?.bannerOverview}
+          {movie?.overview || movie?.bannerOverview}
         </h1>
+        </div>
       </div>
     </header>
   )
