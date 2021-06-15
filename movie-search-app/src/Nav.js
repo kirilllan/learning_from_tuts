ac63a882a8f7415a7b9c8f7c4c8176fe06e567c0
kirilllan <s2100138@edu.bc.fi>
2021-06-15 11:03:41 +0300

    finish nav style

--- movie-search-app/src/Nav.js
+++ movie-search-app/src/Nav.js
@@ -1,19 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
 import "./Nav.css";
 
 function Nav() {
+
+  const [show, handleShow] = useState(false);  
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) handleShow(true)
+      else handleShow(false)
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    }
+  }, []);
+
   return (
-    <div className="nav">
+    <div className={`nav ${show && "nav__black"}`}>
       <img className="nav__logo" 
         src="https://upload.wikimedia.org/wikipedia/commons/5/5f/A_possum_and_a_movie_camera_1943.jpg"
         alt="navigation bar logotypeish"
       />
 
-      <img 
+      {/* <img 
         className="nav__avatar"
         src="https://upload.wikimedia.org/wikipedia/commons/1/12/RZ_Metro_Movie_Theater_2019-07_%284%29.jpg"
         alt="navigation bar minor image"
-      />
+      /> */}
     </div>
   )
 }
