19140a8561f3cf69b60ae967b118af79b75b7579
kirilllan <s2100138@edu.bc.fi>
2021-06-15 13:49:49 +0300

    auto scroll functionality

--- movie-search-app/src/Banner.js
+++ movie-search-app/src/Banner.js
@@ -41,9 +41,10 @@ function Banner() {
           {truncate(movie?.overview, 150)}
         </h1>
         </div>
+        <div className="banner--fadeBottom" />
       </div>
 
-      <div className="banner--fadeBottom" />
+      
     </header>
   )
 }
