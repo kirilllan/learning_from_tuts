cbde80467b69d4deee6c75cde1214b6ad01000ad
kirilllan <s2100138@edu.bc.fi>
2021-06-16 13:13:32 +0300

    connection to api

--- music-app/src/App.js
+++ music-app/src/App.js
@@ -1,9 +1,24 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from "./Login";
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if (_token) {setToken(_token)};
+  }, []);
+
   return (
     <div className="app">
+      {
+        token ? ('fefu') : (<Login />)
+      }
       <Login />
     </div>
   );
