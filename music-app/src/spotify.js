a8cea3b79b7eed790238026900b90eececcd8e0f
kirilllan <s2100138@edu.bc.fi>
2021-06-16 11:33:51 +0300

    d

---
+++ music-app/src/spotify.js
@@ -0,0 +1,17 @@
+//https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+
+export const authEndPoint = "https://accounts.spotify.com/authorize";
+
+const redirectUri = "http://localhost:3000/";
+
+const clientId = "cba08e9e4bd749d4bf78fa682b26770d";
+
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state"
+];
+
+export const loginUrl = `${authEndPoint}?client_id=${clientId}&redirect_uri=${redirectUri}`;
