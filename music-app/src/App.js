2ce302d66e04f6c5c202fb48f03adc9facf3b199
kirilllan <s2100138@edu.bc.fi>
2021-06-17 12:01:02 +0300

    get user from datalayer

--- music-app/src/App.js
+++ music-app/src/App.js
@@ -4,12 +4,14 @@ import Login from "./Login";
 import { getTokenFromUrl } from './spotify';
 import SpotifyWebApi from 'spotify-web-api-js';
 import Player from "./Player";
+import { useDataLayerValue } from './DataLayer';
 
 const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
-
+  const [{ user }, dispatch] = useDataLayerValue();
+  
   useEffect(() => {
     const hash = getTokenFromUrl();
     window.location.hash = "";
@@ -18,10 +20,16 @@ function App() {
       setToken(_token);
       spotify.setAccessToken(_token);
       spotify.getMe().then(user => {
-        console.log('user ███', user)
+         console.log('user ███', user)
+         dispatch({
+           type: 'SET_USER',
+           user: user
+         })
       })
     }
   }, []);
+
+  console.log('user ███', user)
 
   return (
     <div className="app">
