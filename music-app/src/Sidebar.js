d04cc6e2291655fb9999fc0e84127239b80278fb
kirilllan <s2100138@edu.bc.fi>
2021-06-17 14:00:57 +0300

    m-ui icons&condit render

--- music-app/src/Sidebar.js
+++ music-app/src/Sidebar.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import "./sidebar.css";
+import SidebarOption from './SidebarOption';
+
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <img className="sidebar__logo" src="pelican.webp" alt="logo"/>
+      <SidebarOption title="Home" Icon={HomeIcon} />
+      <SidebarOption title="Search" Icon={SearchIcon} />
+      <SidebarOption title="Your Library" Icon={LibraryMusicIcon} />
     </div>
   )
 }
