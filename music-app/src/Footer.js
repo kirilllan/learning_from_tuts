4eb897a99d1d2a0de4afc51142b9554fbb56a448
kirilllan <s2100138@edu.bc.fi>
2021-06-18 11:23:56 +0300

    footer icons&style

--- music-app/src/Footer.js
+++ music-app/src/Footer.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import "./footer.css";
 
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayArrowOutlined";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import { Grid, Slider } from "@material-ui/core";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlayOutlined";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+
 function Footer() {
   return (
     <div className="footer">
-      he am footer
+      <div className="footer__left">
+        <p>Album and song details</p>
+      </div>
+      <div className="footer__center">
+        <ShuffleIcon className="footer__green" />
+        <SkipPreviousIcon className="footer__icon" />
+        <PlayCircleOutlineIcon className="footer__icon" fontSize="large" />
+        <SkipNextIcon className="footer__icon" />
+        <RepeatIcon className="footer__green" />
+      </div>
+      <div className="footer__right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider aria-labelledby="continuous-slider" />
+          </Grid>
+        </Grid>
+      </div>
     </div>
   )
 }
--- music-app/src/Sidebar.js
+++ music-app/src/Sidebar.js
@@ -6,7 +6,11 @@ import HomeIcon from "@material-ui/icons/Home";
 import SearchIcon from "@material-ui/icons/Search";
 import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
 
+import { useDataLayerValue } from './DataLayer';
+
 function Sidebar() {
+  const [{playlists}, dispatch] = useDataLayerValue();
+
   return (
     <div className="sidebar">
       <img className="sidebar__logo" src="pelican.webp" alt="logo"/>
@@ -17,6 +21,10 @@ function Sidebar() {
       <br />
       <strong className="sidebar__title">PLAYLISTS</strong>
       <hr />
+
+      {playlists?.items?.map(playlist => (
+        <SidebarOption title={playlist.name} />
+      ))}
     </div>
   )
 }
