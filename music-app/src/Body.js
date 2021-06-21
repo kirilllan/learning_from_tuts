966b4f5dc57ccf1f43b4592ff038d1bbd2a82be7
kirilllan <s2100138@edu.bc.fi>
2021-06-21 11:14:53 +0300

    banner styles

--- music-app/src/Body.js
+++ music-app/src/Body.js
@@ -6,6 +6,15 @@ function Body({ spotify }) {
   return (
     <div className="body">
       <Header spotify={spotify}/>
+
+      <div className="body__info">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/1/1d/Yandhi_Cover_Art_%28Free_License%29.jpg" alt="playlist cover img" />
+        <div className="body__info_Text">
+          <strong>Playlist</strong>
+          <h2>Discover weekly</h2>
+          <p>description ...</p>
+        </div>
+      </div>
     </div>
   )
 }
