cf6633affcf2ad6dd0956333e6f9a609c54b7023
kirilllan <s2100138@edu.bc.fi>
2021-06-21 13:34:18 +0300

    banner don

--- music-app/src/Body.js
+++ music-app/src/Body.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import "./body.css";
 import Header from "./Header";
+import { useDataLayerValue} from "./DataLayer";
 
 function Body({ spotify }) {
+  const [{discover_weekly}, dispatch] = useDataLayerValue();
+
   return (
     <div className="body">
       <Header spotify={spotify}/>
 
       <div className="body__info">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/1/1d/Yandhi_Cover_Art_%28Free_License%29.jpg" alt="playlist cover img" />
+        <img src={discover_weekly.images[0].url || "https://upload.wikimedia.org/wikipedia/commons/1/1d/Yandhi_Cover_Art_%28Free_License%29.jpg"} alt="playlist cover img" />
         <div className="body__info_Text">
           <strong>Playlist</strong>
           <h2>Discover weekly</h2>
-          <p>description ...</p>
+          <p>{discover_weekly?.description}</p>
         </div>
+      </div>
+      <div className="body__songs">
+        
       </div>
     </div>
   )
--- music-app/src/reducer.js
+++ music-app/src/reducer.js
@@ -19,6 +19,9 @@ const reducer = (state, action) => {
     case 'SET_PLAYLISTS': return {
       ...state, playlists: action.playlists
     }
+    case 'SET_DISCOVER_WEEKLY': return {
+      ...state, discover_weekly: action.discover_weekly
+    }
     default: return state
   }
 }
