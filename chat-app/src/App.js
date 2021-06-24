e0bebefae22df3d645e3329b0dffcc58b0adb0e1
kirilllan <s2100138@edu.bc.fi>
2021-06-24 14:39:44 +0300

    hedr styles don

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="app">
       hallo from app.js
       <Header />
+      <div className="app__body">
+        <Sidebar />
+      </div>
     </div>
   );
 }
