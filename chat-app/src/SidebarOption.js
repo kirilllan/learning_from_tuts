1b5213b3495a4c2eff4d1795a3fb4bcce3fb10af
kirilllan <s2100138@edu.bc.fi>
2021-06-25 11:51:53 +0300

    sidebaropt styles

--- chat-app/src/SidebarOption.js
+++ chat-app/src/SidebarOption.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import "./sidebarOption.css";
 
-
 function SidebarOption({Icon, title}) {
   return (
     <div className="sidebarOption">
       {Icon && <Icon className="sidebarOption__icon" />}
-      {Icon ? <h3>{title}</h3> : <h3><span className="sidebarOption__hash">#</span> {title}</h3>}
+      {Icon ? <h3 className="sidebarOption__channel">{title}</h3> : <h3><span className="sidebarOption__hash">#</span>{title}</h3>}
     </div>
   )
 }
