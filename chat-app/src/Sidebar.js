a341dd0f612e98000cd8c29e4ef7694cd91cfb7e
kirilllan <s2100138@edu.bc.fi>
2021-06-25 10:59:06 +0300

    som sidebar structure&css

--- chat-app/src/Sidebar.js
+++ chat-app/src/Sidebar.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import "./sidebar.css";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecordRounded";
+import CreateIcon from "@material-ui/icons/Create";
 
 function Sidebar() {
   return (
-    <div>
-      
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <div className="sidebar__info">
+          <h2>chat rooms</h2>
+          <h3>
+            <FiberManualRecordIcon />
+            account name
+          </h3>
+        </div>
+        <CreateIcon />
+      </div>
     </div>
   )
 }
