830685f3663e6e08ba7a3bb78976319be91a19e4
kirilllan <s2100138@edu.bc.fi>
2021-06-29 12:30:21 +0300

    Chat styles

--- chat-app/src/Chat.js
+++ chat-app/src/Chat.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import "./chat.css";
+import { useParams } from "react-router-dom";
+
+import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
 
 function Chat() {
+  const { roomId } = useParams();
   return (
-    <div classNAme="chat">
-      
+    <div className="chat">
+      <div className="chat__header">
+        <div className="chat__headeLeft">
+          <h4 className="chat__channelName">
+            <strong>#General</strong>
+            <StarBorderOutlinedIcon />
+          </h4>
+        </div>
+        <div className="chat__headerRight">
+          <p>
+            <InfoOutlinedIcon /> Details
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
