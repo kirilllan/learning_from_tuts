7041bfa0605e2222f1432bda81442c72873c23af
kirilllan <s2100138@edu.bc.fi>
2021-06-30 11:22:46 +0300

    Message timestamp&styles

--- chat-app/src/Message.js
+++ chat-app/src/Message.js
@@ -7,7 +7,9 @@ function Message({ message, timestamp, user, userImage }) {
       <img src={userImage} alt="small image of user" />
       <div className="message__info">
         <h4>
-          {user} {timestamp}
+          {user} <span className="message__timestamp">
+          {new Date(timestamp?.toDate()).toUTCString()}
+          </span>
         </h4>
         <p>{message}</p>
       </div>
