20348dd023808f10d69b61ad4e3b870474f655ab
kirilllan <s2100138@edu.bc.fi>
2021-06-30 10:41:53 +0300

    chng roomname dynamicly

--- chat-app/src/Chat.js
+++ chat-app/src/Chat.js
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import "./chat.css";
 import { useParams } from "react-router-dom";
+import db from "./firebase";
 
 import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
 import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import { useEffect } from 'react/cjs/react.development';
 
 function Chat() {
   const { roomId } = useParams();
+  const [roomDetails, setRoomDetails] = useParams(null);
+  useEffect(() => {
+    if (roomId) {
+      db.collection('rooms').doc(roomId).onSnapshot(snapshot => (
+        setRoomDetails(snapshot.data())
+      ))
+    }
+  }, [roomId])
   return (
     <div className="chat">
       <div className="chat__header">
         <div className="chat__headeLeft">
           <h4 className="chat__channelName">
-            <strong>#General</strong>
+            <strong>#{roomDetails?.name}</strong>
             <StarBorderOutlinedIcon />
           </h4>
         </div>
