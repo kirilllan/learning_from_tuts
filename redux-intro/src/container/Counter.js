5b7a9c923ed8bcdaeb733756a9ff94c5c1b105d0
kirilllan <s2100138@edu.bc.fi>
2021-08-16 12:53:41 +0300

    mapStateToProps

--- redux-intro/src/container/Counter.js
+++ redux-intro/src/container/Counter.js
@@ -5,14 +5,20 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        <h1>Your score is: 0</h1>
+        <h1>Your score is: {this.props.ctr}</h1>
         <div>
-          <button>+1</button>
-          <button>-1</button>
+          <button>+</button>
+          <button>-</button>
         </div>
       </div>
     )
   }
 }
 
-export default Counter;
+const mapStateToProps = state => {
+  return {
+    ctr: state.counter
+  }
+}
+
+export default connect(mapStateToProps)(Counter);
