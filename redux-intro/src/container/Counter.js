a456e8c7b57bc982892893631b8efa3aadb898b2
kirilllan <s2100138@edu.bc.fi>
2021-08-17 12:26:25 +0300

    displ of stored items

--- redux-intro/src/container/Counter.js
+++ redux-intro/src/container/Counter.js
@@ -15,6 +15,12 @@ class Counter extends Component {
           <button onClick={this.props.onRemove}>-5</button>
           <button onClick={this.props.resetCounter}>reset</button>
         </div>
+        <button>Store the results</button>
+        <div>
+          <ul>
+            {this.props.storedResults.map(item => <li key={item.id}>{item.value}</li>)}
+          </ul>
+        </div>
       </div>
     )
   }
@@ -22,7 +28,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
   return {
-    ctr: state.counter
+    ctr: state.counter,
+    storedResults: state.results
   }
 }
 
@@ -30,7 +37,7 @@ const mapDispatchToProps = dispatch => {
   return {
     onIncCounter: () => dispatch({ type: actionTypes.INCREMENT }),
     onDecCounter: () => dispatch({ type: actionTypes.DECREMENT }),
-    onAdd: () => dispatch({ type: actionTypes.ADD, valie: 5 }),
+    onAdd: () => dispatch({ type: actionTypes.ADD, value: 5 }),
     onRemove: () => dispatch({ type: actionTypes.REMOVE, value: 5 }),
     resetCounter: () => dispatch({ type: actionTypes.RESET })
   }
--- redux-intro/src/reducers/reducer.js
+++ redux-intro/src/reducers/reducer.js
@@ -5,19 +5,22 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.INCREMENT: return { ...state, counter: state.counter + 1 }
     case actionTypes.DECREMENT: return { ...state, counter: state.counter - 1 }
+    case actionTypes.ADD: return { ...state, counter: state.counter + action.value }
+    case actionTypes.REMOVE: return { ...state, counter: state.counter - action.value }
     case actionTypes.RESET: return { ...state, counter: 0 }
-    case actionTypes.ADD: return {
-      ...state, counter: state.counter + action.value
-    }
-    case actionTypes.REMOVE: return {
-      ...state, counter: state.counter - action.value
-    }
   }
   return state
 }
 
 const initialState = {
-  counter: 0
+  counter: 0,
+  results: [{
+    id: 1,
+    value: 10
+  }, {
+    id: 2,
+    value: 20
+  }]
 }
 
 export default reducer;
